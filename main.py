4fe278f60c5ad82163091edf8fa196eff488c006
Abu Hurayra <hurayraiit@gmail.com>
2022-01-29 19:14:34 +0600

    preparing to call smescan

--- main.py
+++ main.py
@@ -1,19 +1,49 @@
 from flask import Flask
+from flask import request, escape
+from smescan import *
+
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "Hello world!"
+    
+    URL = str(escape(request.args.get("website", "")))
+    USERNAME = str(escape(request.args.get("email", "")))
+    PASSWORD = str(escape(request.args.get("password", "")))
+    
+    if URL and USERNAME and PASSWORD:
+        OUTPUT = RunSmeScan(URL, USERNAME, PASSWORD)
+    else:
+        OUTPUT = "empty"
+    
+    
+    return (
+        """
+        <form action="" method="get" autocomplete="on">
+        
+            <p>Select Website:</p>
+            
+            <input type="radio" id="testing" name="website" value="https://testing.smevai.com" checked>
+            <label for="testing">Testing</label>
+            <input type="radio" id="app" name="website" value="https://app.smevai.com">
+            <label for="app">App</label><br> <br>
+            
+            
+            <p>User Email:</p>
+            <input type="email" name="email" value="wpdabh+year22ta1@gmail.com" required> <br><br>
+            
+            <p>User Password:</p>
+            <input type="text" name="password" value="pass1234" required> <br><br>
+            
+            <input type="submit" value="send">
+        </form>
+        """
+        + OUTPUT
+    )
 
-@app.route("/<celsius>")
-def fahrenheit_from(celsius):
-    try:
-        fahrenheit = float(celsius) * 9 / 5 + 32
-        fahrenheit = round(fahrenheit, 3)
-        return str(fahrenheit)
-    except ValueError:
-        return "Invalid Input: Please Enter a number"
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
+    
